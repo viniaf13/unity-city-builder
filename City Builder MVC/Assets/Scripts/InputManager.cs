c1590a01f99ef696a67557a866f8782641a811b1
viniaf13 <viniaf13@gmail.com>
2021-01-11 16:58:37 -0300

    setup input manager

---
+++ City Builder MVC/Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InputManager : MonoBehaviour
+{
+    public LayerMask mouseInputMask;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        GetPlayerInput();
+    }
+
+    public void GetPlayerInput()
+    {
+        //TODO: Compatible with other devices
+        //If left button is pressed and it is not on UI
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            GetPointClicked();
+        }
+    }
+
+    private void GetPointClicked()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
+        {
+            Vector3 position = hit.point - transform.position;
+            Debug.Log(position);
+        }
+    }
+}
