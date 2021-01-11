da3a4979927b7bf9775c102813de1db3098011f0
viniaf13 <viniaf13@gmail.com>
2021-01-11 18:03:21 -0300

    created grid structure class and unit tests for it

---
+++ City Builder MVC/Assets/EditModeTests/GridStructureTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class GridStructureTests
+    {
+        GridStructure gridStructure;
+        [OneTimeSetUp]
+        public void Init()
+        {
+            gridStructure = new GridStructure(3);
+        }
+
+        [Test]
+        public void CalculateGridPositionPasses()
+        {
+            Vector3 position = new Vector3(0, 0, 0);
+            Vector3 returnedPosition = gridStructure.CalculateGridPosition(position);
+            Assert.AreEqual(Vector3.zero, returnedPosition);
+        }
+
+        [Test]
+        public void CalculateGridPositionFails()
+        {
+            Vector3 position = new Vector3(3.1f, 0, 0);
+            Vector3 returnedPosition = gridStructure.CalculateGridPosition(position);
+            Assert.AreNotEqual(Vector3.zero, returnedPosition);
+        }
+
+        [Test]
+        public void CalculateGridPositionFloatPasses()
+        {
+            Vector3 position = new Vector3(2.9f, 0, 2.9f);
+            Vector3 returnedPosition = gridStructure.CalculateGridPosition(position);
+            Assert.AreEqual(Vector3.zero, returnedPosition);
+        }
+
+    }
+}
--- City Builder MVC/Assets/Scripts/InputManager.cs
+++ City Builder MVC/Assets/Scripts/InputManager.cs
@@ -5,11 +5,10 @@ using UnityEngine.EventSystems;
 
 public class InputManager : MonoBehaviour
 {
-    public LayerMask mouseInputMask;
-    void Start()
-    {
-        
-    }
+    [SerializeField] LayerMask mouseInputMask;
+    //[SerializeField] GameObject buildingPrefab;
+    //[SerializeField] int cellSize = 3;
+
     void Update()
     {
         GetPlayerInput();
@@ -21,18 +20,32 @@ public class InputManager : MonoBehaviour
         //If left button is pressed and it is not on UI
         if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
         {
-            GetPointClicked();
+            CalculateInputPosition();
         }
     }
 
-    private void GetPointClicked()
+    private void CalculateInputPosition()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
         if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, mouseInputMask))
         {
-            Vector3 position = hit.point - transform.position;
-            Debug.Log(position);
+            Vector3 mousePosition = hit.point - transform.position;
+            //Vector3 gridPosition = CalculateGridPosition(mousePosition);
+            //CreateBuilding(gridPosition);
         }
     }
+
+    /*private Vector3 CalculateGridPosition(Vector3 inputPosition)
+    {
+        int x = Mathf.FloorToInt((float)inputPosition.x / cellSize);
+        int z = Mathf.FloorToInt((float)inputPosition.z / cellSize);
+        return new Vector3(x*cellSize, 0, z*cellSize);
+    }
+
+    private void CreateBuilding(Vector3 gridPosition)
+    {
+        GameObject building = Instantiate(buildingPrefab, gridPosition, Quaternion.identity) as GameObject;
+        //building.transform.parent
+    }*/
 }
